03764a02c0f30d8b231dbc1e16bdb6980cbc8357
종윤 <jongyoon9515@gmail.com>
2021-02-27 20:12:15 +0900

    CyclicRotation
    Rotate an array to the right by a given number of steps.

---
+++ CyclicRotation.js
@@ -0,0 +1,15 @@
+/**
+ * Rotate an array to the right by a given number of steps.
+ * @param {*} A
+ * @param {*} K
+ */
+function solution(A, K) {
+    let arr = A.length == 0 ? [] : A; // empty array check
+
+    for(let i = 0; i < K; i++) {
+        let last = arr.pop(); // 배열의 마지막 요소 제거
+        arr.unshift(last);    // 배열의 처음에 요소 추가
+    }
+
+    return A;
+}
